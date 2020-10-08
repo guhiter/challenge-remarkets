c3e0d230d98b21919969b00c8b094c37df0bd37a
Nicolas Bosio <nicolas.bosio@mangoodev.com>
2020-10-08 17:25:39 -0300

    minor string changes in arguments

--- argsParser.py
+++ argsParser.py
@@ -4,18 +4,18 @@ from constants import defaultBIDPrice, defaultBIDDif
 def parseArgs():
     parser = argparse.ArgumentParser(description=
         'Consultar sobre Remarkets el BID de un símbolo e ingresar una orden ' +
-        'de compra a ${:.2f} menos de la ultima entrada o '.format(defaultBIDDif) +
+        'de compra a ${:.2f} menos de la última entrada o '.format(defaultBIDDif) +
         'a ${:.2f} en caso que no haya ninguna'.format(defaultBIDPrice),
         add_help=True)
     parser.add_argument('-s', '--symbol', required=True,
-        help='Simbolo a consultar en el mercado (Remarkets), ' +
+        help='Símbolo a consultar en el mercado (Remarkets), ' +
         'y posteriormente colocar una orden de compra',
         dest='symbol')
     parser.add_argument('-u', '--user', required=True,
-        help='Usuario de la cuenta de reMarkets para utilizar la API',
+        help='Usuario de la cuenta de Remarkets para utilizar la API',
         dest='apiUser')
     parser.add_argument('-p', '--password', required=True,
-        help='Contraseña de la cuenta de reMarkets para utilizar la API',
+        help='Contraseña de la cuenta de Remarkets para utilizar la API',
         dest='apiPass')
     parser.add_argument('--price', default=defaultBIDPrice, type=float,
         help='Valor al cual se debe colocar la orden de compra en ' +
@@ -26,3 +26,6 @@ def parseArgs():
         'en caso que exista al menos una entrada',
         dest='bidDif')
     return parser.parse_args()
+
+if __name__ == "__main__":
+    parseArgs()
